57eaac0f622b53050d0f742b393f1b8db66c70e0
Benjamin Owad <jurplel@interversehq.com>
2024-02-26 01:33:42 -0500

    Delete .config/fish/functions/conn-dev.fish

--- .config/fish/functions/conn-dev.fish
+++
@@ -1,4 +0,0 @@
-function conn-dev --wraps='ssh bowad@bowad.duckdns.org -p 21322 -X' --wraps='ssh bowad@shark.ics.cs.cmu.edu' --description 'alias conn-dev=ssh bowad@bowad.duckdns.org -p 21322 -X'
-  ssh bowad@bowad.duckdns.org -p 21322 -C $argv
-        
-end
